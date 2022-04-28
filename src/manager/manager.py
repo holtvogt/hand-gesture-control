58e8a577c7c4fbb1f0dbc2c74bd29e745ecb72b6
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2022-04-28 17:33:20 +0200

    Add video capture check method
    Also add method to check if the exit button was pressed

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -1,23 +1,41 @@
 from abc import abstractmethod
 
-from cv2 import VideoCapture, destroyAllWindows
+from cv2 import VideoCapture, destroyAllWindows, waitKey
 
+from src.model.button import Button
 from src.tracker.tracker import Tracker
 
 
 class Manager:
     _DEFAULT_CAMERA: int = 0
+    _EXIT_BUTTON: int = Button.ESCAPE.value
+    _MINIMUM_WAIT: int = 10
 
     _tracker: Tracker
     _capture: VideoCapture
 
     @abstractmethod
     def start(self):
-        """Starts the controller."""
+        """Starts the manager."""
 
     def stop(self):
+        """Stops the manager."""
+
         self._capture.release()
         destroyAllWindows()
 
-    def has_camera(self):
+    def video_capturing_enabled(self):
+        """Checks whether video capturing via camera enabled.
+
+        :return: True, if video capturing is successfully initialized, false otherwise
+        """
+
         return self._capture.isOpened()
+
+    def _exit_was_pressed(self) -> bool:
+        """Checks if the corresponding exit button was pressed to close video capturing.
+
+        :return: True, if the button was pressed, false otherwise
+        """
+
+        return (waitKey(self._MINIMUM_WAIT) & 0xFF) == self._EXIT_BUTTON
